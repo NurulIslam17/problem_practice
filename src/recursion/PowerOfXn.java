37e5f3ba504285cd4c183914c8de97e91714a046
NurulIslam17 <nurulcse09@gmail.com>
2025-04-26 20:48:21 +0600

    Rfactor

--- src/recursion/PowerOfXn.java
+++ src/recursion/PowerOfXn.java
@@ -4,9 +4,11 @@ public class PowerOfXn {
 
     public static double solution(double x, int n) {
         double mult = 1;
-        for (int i = 1; i <=n ; i++) {
+        int range = Math.abs(n);
+        for (int i = 1; i <=range ; i++) {
             mult=mult*x;
         }
+        System.out.println(mult);
         return mult;
     }
 
@@ -18,8 +20,8 @@ public class PowerOfXn {
     }
 
     public static void main(String[] args) {
-        double x = 2.100;
-        int n = 3;
+        double x =  2.00000;
+        int n = -2;
         System.out.println(powXn(x, n));
     }
 }
