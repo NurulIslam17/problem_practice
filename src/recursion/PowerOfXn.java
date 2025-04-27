24770619a991775910711d203201858a607e5948
NurulIslam17 <nurulcse09@gmail.com>
2025-04-27 22:00:37 +0600

    Power of XN

--- src/recursion/PowerOfXn.java
+++ src/recursion/PowerOfXn.java
@@ -2,21 +2,39 @@ package recursion;
 
 public class PowerOfXn {
 
-    public static double solution(double x, int n) {
-        double mult = 1;
-        int range = Math.abs(n);
-        for (int i = 1; i <=range ; i++) {
-            mult=mult*x;
+    public static double solution(double x, long n) {
+        if (n == 0) {
+            return 1.00;
         }
-        System.out.println(mult);
-        return mult;
+        double half = solution(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
     }
 
+//    public static double solution(double x, long n) {
+//        double mult = 1;
+//        int range = (int) Math.abs(n) / 2;
+//        for (int i = 1; i <= range; i++) {
+//            mult = mult * x;
+//        }
+//        if (n % 2 == 0) {
+//            return mult * mult;
+//        } else {
+//            return mult * mult * x;
+//        }
+//    }
+
+
     public static double powXn(double x, int n) {
-        if (n > 0) {
-            return solution(x, n);
+        long N = n;
+        if (N > 0) {
+            return solution(x, N);
         }
-        return 1 / solution(x, n);
+        return 1 / solution(x, N);
     }
 
     public static void main(String[] args) {
