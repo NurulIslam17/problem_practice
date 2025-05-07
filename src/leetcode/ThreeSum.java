3119489bcc42fd9eff815b2712c4befd520dfb2a
NurulIslam17 <nurulcse09@gmail.com>
2025-05-07 22:10:05 +0600

    Three Sum

--- src/leetcode/ThreeSum.java
+++ src/leetcode/ThreeSum.java
@@ -1,15 +1,31 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 public class ThreeSum {
-
-    private static int calculateThreeSum(int[] arr, int target) {
-
-        return target;
+    private static void calculateThreeSum(int[] arr) {
+        HashSet<List<Integer>> result = new HashSet<>();
+        for (int i = 0; i < arr.length - 2; i++) {
+            int left = i + 1;
+            int right = arr.length - 1;
+            int sum = arr[i] + arr[left] + arr[right];
+            while (left < right) {
+                if (sum == 0) {
+                    result.add(Arrays.asList(arr[i], arr[left], arr[right]));
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
-        int[] arr = {-1, 2, 1, -4};
-        int target = 1;
-        System.out.println(calculateThreeSum(arr, target));
+        int[] arr = {-4, -1, -1, 0, 1, 2};
+        calculateThreeSum(arr);
     }
 }
