9b83aa5dfd06a39e33f96efd77500b98db435cbe
NurulIslam17 <nurulcse09@gmail.com>
2025-05-15 19:59:31 +0600

    Permutation II init

---
+++ src/leetcode/PermutationII.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermutationII {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        calculatePermutation(nums);
+    }
+
+    private static void calculatePermutation(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+
+        System.out.println("jfdhd");
+    }
+}
